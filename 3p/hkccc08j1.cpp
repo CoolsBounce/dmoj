c03921eef8b558b9ef364da8cb0ec6742641c060
Alex Gan <42101070+CoolsBounce@users.noreply.github.com>
2021-09-18 19:16:38 -0400

    Create hkccc08j1.cpp

---
+++ 3p/hkccc08j1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int c, n; cin >> c;
+    int cmax, nmax = 0;
+    for (int i = 0, a, b; i < c; i++) {
+        cin >> a >> b;
+        cmax = max(cmax, a*b);
+    }
+    cin >> n;
+    for (int i = 0, a, b; i < n; i++) {
+        cin >> a >> b;
+        nmax = max(nmax, a*b);
+    }
+    if (cmax > nmax) cout << "Casper\n";
+    else if (nmax > cmax) cout << "Natalie\n";
+    else cout << "Tie\n";
+}
