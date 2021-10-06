3d020fda39341bf7cf9a36e8770b99ba3e5fd679
Alex Gan <42101070+CoolsBounce@users.noreply.github.com>
2021-10-06 10:38:09 -0400

    Create fhc15c1p1.cpp

---
+++ 10p/fhc15c1p1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int primacity[10000001];
+bool isPrime[10000001];
+int n = 10000000;
+
+vector<int> nums[20];
+int main() {
+    memset(isPrime, true, sizeof(isPrime));
+    for (int i = 2; i*i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i*i; j <= n; j += i) isPrime[j] = false;
+        }
+    }
+    for (int i = 2; i <= n; i++) {
+        if (primacity[i] == 0) {
+            for (int j = i; j <= n; j += i) primacity[j]++;
+        }
+    }
+    for (int i = 2; i <= n; i++) nums[primacity[i]].push_back(i);
+    int t; cin >> t;
+    for (int i = 1, a, b, k; i <= t; i++) {
+        cin >> a >> b >> k;
+        int ans = upper_bound(nums[k].begin(), nums[k].end(), b) - lower_bound(nums[k].begin(), nums[k].end(), a);
+        cout << "Case #" << i << ": " << ans << "\n";
+    }
+}
